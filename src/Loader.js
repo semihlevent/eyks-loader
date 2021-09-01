30e733250236c09ee451dd3a8cde339c2ea7ca2a
semihlevent <semihlt@hotmail.com>
2021-09-02 01:52:33 +0300

    status bar added

--- src/Loader.js
+++ src/Loader.js
@@ -2,11 +2,16 @@
 
 const Loader = () => {
     return (
-        <div className="eyks-loader-container">
-            <div className="tri-up"></div>
-            <div className="tri-up-white"></div>
-            <div className="tri-down"></div>
-            <div className="tri-down-white"></div>
+        <div className="eyks-loader">
+            <div className="eyks-arrow">
+                <div className="tri-up"></div>
+                <div className="tri-up-white"></div>
+                <div className="tri-down"></div>
+                <div className="tri-down-white"></div>
+            </div>
+            <div className="status-bar-container">
+                <div className="status-bar"></div>
+            </div>
         </div>
     );
 }
