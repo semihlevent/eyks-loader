76b09be978128f883b03493c6446249c3468e1aa
semihlevent <semihlt@hotmail.com>
2022-01-02 13:28:01 +0300

    BoardMembersLoader Added

---
+++ src/lib/BoardMembersLoader.js
@@ -0,0 +1,131 @@
+import React from 'react';
+import styled, { keyframes, css } from 'styled-components';
+import World from './World';
+
+
+
+
+{/* <div className='world__container' style={{marginTop: '1em'}}>
+                <div className='world__vertical'></div>
+                <div className='world__horizontal'></div>
+                <div className='world__circle'></div>
+                <div className='world__circle' style={{transform: 'rotateY(30deg)'}}></div>
+                <div className='world__circle' style={{transform: 'rotateY(60deg)'}}></div>
+                <div className='world__circle' style={{transform: 'rotateY(90deg)'}}></div>
+                <div className='world__circle' style={{transform: 'rotateY(120deg)'}}></div>
+                <div className='world__circle' style={{transform: 'rotateY(150deg)'}}></div>
+            </div> */}
+
+const BoardMembersLoader = props => {
+    const { size, colorPrimary="#6c6d6f", colorSecondary="#971f20", backgroundColor="white",
+        spin, spinDuration="90s", worldSpin, worldSpinDuration
+    } = props;
+
+    const passingProps = {size, colorPrimary, colorSecondary, backgroundColor, spin, spinDuration };
+    const worldProps = {color: colorPrimary, spin: worldSpin, spinDuration: worldSpinDuration};
+    //size: 3.2 6.8
+    return (
+        <Container {...passingProps}>
+            <Chamber {...passingProps}>
+                <Round backgroundColor={backgroundColor} color={colorPrimary} style={{top: '.8em', transform: 'translateY(-50%)'}} />
+                <Round backgroundColor={backgroundColor} color={colorSecondary} style={{bottom: '.8em', transform: 'translateY(50%) rotate(180deg)'}} />
+                <Round backgroundColor={backgroundColor} color={colorSecondary} style={{left: '3.7em', top:'5.1em', transform: 'translateX(-50%) rotate(300deg)'}} />
+                <Round backgroundColor={backgroundColor} color={colorSecondary} style={{left: '3.7em', bottom:'5.1em', transform: 'translateX(-50%) rotate(240deg)'}} />
+                <Round backgroundColor={backgroundColor} color={colorSecondary} style={{right: '3.7em', top:'5.1em', transform: 'translateX(50%) rotate(60deg)'}} />
+                <Round backgroundColor={backgroundColor} color={colorSecondary} style={{right: '3.7em', bottom:'5.1em', transform: 'translateX(50%) rotate(120deg)'}} />
+            </Chamber>
+            <World {...worldProps} />
+        </Container>
+    );
+};
+
+export default BoardMembersLoader;
+
+// https://stackoverflow.com/questions/11644993/want-to-show-the-thickness-of-an-element-while-it-rotate
+
+// https://www.youtube.com/watch?v=FeJEEE3zc4U
+
+
+const Container = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: ${props => `${props.size}px`};
+    width: 40em;
+    height: 40em;
+`;
+
+const Round = styled.div`
+    & {
+        width: 10em;
+        height: 10em;
+        position: absolute;
+        background-color: ${props => props.backgroundColor};
+        border-radius: 50%;
+    }
+    &::after {
+        content: "";
+        position: absolute;
+        border-radius: 50%;
+        background-color: ${props => props.color};
+        width: 8.8em;
+        height: 8.8em;
+        top: 50%;
+        left: 50%;
+        transform: translateX(-50%) translateY(-50%);
+    }
+    &::before {
+        content: "";
+        position: absolute;
+        background-color:${props => props.backgroundColor};
+        width: 0.65em;
+        height: 4em;
+        top: 100%;
+        left: 50%;
+        transform: translateX(-50%) translateY(-50%);
+    }
+`;
+
+const spin = keyframes`
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+`;
+
+const Chamber = styled.div`
+    & {
+        position: absolute;
+        z-index: -2;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        border-radius: 50%;
+        background: ${({ colorPrimary, colorSecondary }) => `linear-gradient(30deg, ${colorSecondary} 38%, ${colorPrimary} 38%) 0 0,
+        linear-gradient(-30deg, ${colorSecondary} 38%, ${colorPrimary} 38%) 100% 1%,
+        linear-gradient(0deg, ${colorSecondary} 0%, ${colorSecondary} 0%) 0 99%,
+        linear-gradient(0deg, ${colorSecondary} 0%, ${colorSecondary} 0%) 100% 100%`};
+        background-repeat: no-repeat;
+        background-size: 50% 50%;
+        animation: ${props => props.spin && css`${spin} linear infinite`};
+        animation-duration: ${props => props.spin ? props.spinDuration : "0s" };
+    }
+    
+    &::after {
+        width: 26.66em;
+        height: 26.66em;
+        content: "";
+        z-index: -1;
+        position: absolute;
+        background-color: ${props => props.backgroundColor};
+        border-radius: 50%;
+    }
+
+`;
---
+++ src/lib/World.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import styled, { keyframes, css } from 'styled-components';
+
+//eyks red: #971f20 //rgba(151,31,32,255)
+//https://stackoverflow.com/questions/14184494/segments-in-a-circle-using-css3
+
+const World = props => {
+    const { size, thickness="1.5em", color="#6c6d6f", spin, spinDuration="50s" } = props;
+    const passingProps = { size, thickness, color, spin, spinDuration };
+    return (
+        <WorldContainer {...passingProps}>
+            <WorldVerticalLine {...passingProps} />
+            <WorldHorizontalLine {...passingProps} />
+            <WorldCircle1 {...passingProps} />
+            <WorldCircle2 {...passingProps} />
+            <WorldCircle3 {...passingProps} />
+            <WorldCircle4 {...passingProps} />
+            <WorldCircle5 {...passingProps} />
+            <WorldCircle6 {...passingProps} />
+            <WorldCircle7 {...passingProps} />
+        </WorldContainer>
+    );
+};
+
+export default World;
+
+
+const spin = keyframes`
+    0% {
+        transform: rotateY(0deg);
+    }
+    100% {
+        transform: rotateY(360deg);
+    }
+`;
+
+
+const WorldContainer = styled.div`
+    position: relative;
+	transform-style: preserve-3d;
+	width: 20em;
+	height: 20em;
+	font-size: ${props => `${props.size}px`};
+    animation: ${props => props.spin && css`${spin} linear infinite`};
+    animation-duration: ${props => props.spin ? props.spinDuration : "0s" };
+`;
+
+const WorldVerticalLine = styled.div`
+    &{
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        transform-style: preserve-3d;
+    }
+    &::after {
+        content: "";
+        background-color: ${props => props.color};
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: ${props => `calc(50% - calc(${props.thickness} / 2 * .8))`};
+        width: ${props => `calc(${props.thickness} * .8)`};
+        z-index: -10;
+    }
+    &::before {
+        content: "";
+        background-color:${props => props.color};
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left:  ${props => `calc(50% - calc(${props.thickness} / 2 * .8))`};
+        width: ${props => `calc(${props.thickness} * .8)`};
+        transform: rotateY(-90deg);
+        z-index: -10;
+    }
+`;
+
+const WorldHorizontalLine = styled.div`
+    &{
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        transform-style: preserve-3d;
+    }
+    &::after {
+        content: "";
+        background-color: ${props => props.color};
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: ${props => `calc(50% - calc(${props.thickness} / 2 * .8))`};
+        height: ${props => `calc(${props.thickness} * .8)`};
+        z-index: -10;
+    }
+    &::before {
+        content: "";
+        background-color: ${props => props.color};
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: ${props => `calc(50% - calc(${props.thickness} / 2 * .8))`};
+        height: ${props => `calc(${props.thickness} * .8)`};
+        transform: rotateY(-90deg);
+        z-index: -10;
+    }
+`;
+
+const WorldCircle1 = styled.div`
+    position: absolute;
+	transform-style: preserve-3d;
+    top: 0;
+	left: 0;
+    width: ${props => `calc(20em - ${props.thickness} - ${props.thickness})`};
+    height: ${props => `calc(20em - ${props.thickness} - ${props.thickness})`};
+    border-radius: 100%;
+	border: ${({ thickness, color }) => `solid ${thickness} ${color}`};
+    background-color: transparent;
+`;
+
+const WorldCircle2 = styled(WorldCircle1)`
+    transform: rotateY(30deg);
+`;
+
+const WorldCircle3 = styled(WorldCircle1)`
+    transform: rotateY(60deg);
+`;
+
+const WorldCircle4 = styled(WorldCircle1)`
+    transform: rotateY(90deg);
+`;
+
+const WorldCircle5 = styled(WorldCircle1)`
+    transform: rotateY(120deg);
+`;
+
+const WorldCircle6 = styled(WorldCircle1)`
+    transform: rotateY(150deg);
+`;
+
+const WorldCircle7 = styled(WorldCircle1)`
+    transform: rotateY(180deg);
+`;
